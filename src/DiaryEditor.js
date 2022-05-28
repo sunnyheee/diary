c2859f04c2a9dff96f2eb0b6ed651f4847689e5d
Park Seonhui <fool0116@naver.com>
2022-05-28 15:19:39 +0900

    Processing user input

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,7 +1,57 @@
+import { useState } from "react";
+
 const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    console.log(state);
+    alert("Success save!");
+  };
+
   return (
     <div className="DiaryEditor">
       <h2>Today's Diary</h2>
+      <div>
+        <input
+          value={state.author}
+          onChange={handleChangeState}
+          name="author"
+        />
+      </div>
+      <div>
+        <textarea
+          value={state.content}
+          onChange={handleChangeState}
+          name="content"
+        />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>SAVE</button>
+      </div>
     </div>
   );
 };
